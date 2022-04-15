ad7677d58eba7b726f484b992edce14097a7dff5
Rohit Kumar <itsrohit02@gmail.com>
2022-04-15 17:04:10 +0530

    Updated Encode_QR2

--- Encode_QR2.py
+++ Encode_QR2.py
@@ -8,6 +8,6 @@ qr.add_data(data)    # This method is used to add data to the QRCode object. It 
 
 qr.make(fit=True)   # This method with (fit=True) ensures that the entire dimension of the QR Code is utilized, even if our input data could fit into less number of boxes.
 
-img = qr.make_image(fill_color='cyan', back_color='white')  # This method is used to convert the QRCode object into an image file. 
+img = qr.make_image(fill_color='blue', back_color='white')  # This method is used to convert the QRCode object into an image file. 
                                                             # It takes the fill_color and back_color optional parameters to set the foreground and background color.
 img.save('C:/Programming/Projects/QR-Code-Encoder-Decoder/myqrcode2.png')
